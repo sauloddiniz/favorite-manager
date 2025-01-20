affb63d2aaa13947331b6acfe4ddda47c1562e23
Saulo Dias Diniz <sauloddiniz@gmail.com>
2025-01-20 15:08:37 -0300

    Add clientId field to ClientAndListFavoritesResponseDTO
    This update introduces the clientId field to the DTO, ensuring the client's unique identifier is included in responses. It enhances the DTO's functionality by providing more contextual information alongside the existing fields. This change impacts the toResponse method to include the new field.

--- src/main/java/br/com/favoritemanager/adapter/input/DTO/ClientAndListFavoritesResponseDTO.java
+++ src/main/java/br/com/favoritemanager/adapter/input/DTO/ClientAndListFavoritesResponseDTO.java
@@ -7,10 +7,13 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-public record ClientAndListFavoritesResponseDTO(String name, String email, List<ProductResponseDTO> favoritesProducts) {
+public record ClientAndListFavoritesResponseDTO(Long clientId, String name, String email, List<ProductResponseDTO> favoritesProducts) {
 
     public static ClientAndListFavoritesResponseDTO toResponse(Client client) {
-        return new ClientAndListFavoritesResponseDTO(client.getName(), client.getEmail(),
+        return new ClientAndListFavoritesResponseDTO(
+                client.getId(),
+                client.getName(),
+                client.getEmail(),
                 Optional.ofNullable(client.getFavoriteProducts())
                         .orElse(Set.of())
                         .stream()
